34e1701e4ae00dd6c7f734356e647213512df39a
Gokul <87967127+Gokul-S007@users.noreply.github.com>
2022-10-19 23:44:42 -0700

    Delete Tasks directory

--- Tasks/Anto A Team-Member-4/flask-with-sqlite/sqlite_db_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('signup_database.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE users (name TEXT, username TEXT, address TEXT, password TEXT)')
-print("Table created successfully")
-conn.close()
